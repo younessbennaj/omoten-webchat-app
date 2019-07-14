481afaa32830dd556dec6d5d0ec5db0842eb29fe
Youness BENNAJ <youness.bennaj@gmail.com>
2019-07-14 12:27:51 -0400

    add card and carousel component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+    flex: 1;
+    min-width: 16rem;
+    max-width: 16rem;
+`;
+
+const Card = ({ card: { title, text, image, buttons } }) => {
+    return (
+        <CardContainer className="card shadow-sm mx-3">
+            <img src={image} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{text}</p>
+            </div>
+            <ul className="list-group list-group-flush">
+                {buttons.map(button => {
+                    return (
+                        <li className="list-group-item text-center">
+                            <a href="#" className="text-center">{button.title}</a>
+                        </li>
+                    )
+                })}
+            </ul>
+        </CardContainer>
+    );
+}
+
+export default Card;
---
+++ src/components/carousel/SliderContainer.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const SliderContainer = styled.div`
+    position: relative;
+    height: 388px;
+    width: 350px;
+    overflow: hidden;
+`;
+
+export default SliderContainer;
