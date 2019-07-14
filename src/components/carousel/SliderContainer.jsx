cc70e96eb0f2fffa2b48ff680f0853213d41c3c1
Youness BENNAJ <youness.bennaj@gmail.com>
2019-07-14 13:43:22 -0400

    update width and height of slider container

--- src/components/carousel/SliderContainer.jsx
+++ src/components/carousel/SliderContainer.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const SliderContainer = styled.div`
     position: relative;
-    height: 388px;
+    height: 390px;
     width: 350px;
     overflow: hidden;
 `;
