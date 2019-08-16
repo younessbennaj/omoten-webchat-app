c81958d18293679388b73cdaa08fc1454ab15df2
Youness BENNAJ <youness.bennaj@gmail.com>
2019-08-16 18:17:02 -0400

    fix display bug for the quick replies in the messenger window

--- src/containers/messenger/Messenger.jsx
+++ src/containers/messenger/Messenger.jsx
@@ -6,11 +6,10 @@ import styled from 'styled-components';
 import MessageList from '../../components/message-list/MessageList';
 import { ThemeProvider } from 'styled-components';
 import MessengerHeader from '../../components/messenger-header';
+import MessengerWindow from '../../components/MessengerWindow';
 import MessengerInput from '../../components/MessengerInput';
 import QuickReplies from '../../components/QuickReplies';
 import { Box, Flex, Avatar, Heading, Text, Input } from '../../components/UI';
-
-//Messenger Container Style 
 
 const MessengerContainer = styled(Flex)({
     flexDirection: 'column',
@@ -23,68 +22,10 @@ const MessengerContainer = styled(Flex)({
     overflow: 'hidden'
 })
 
-//Message List Container Style 
-
-const MessageListContainer = styled(Flex)({
-    position: 'relative',
-    height: '100%',
-    flexGrow: '1',
-    flexShrink: '1'
-});
-
-MessageListContainer.defaultProps = {
-    bg: "white"
-};
-
-//Message List Content Style 
-
-const MessageListContentContainer = styled(Flex)({
-    flexDirection: 'column',
-    position: 'absolute',
-    top: '0',
-    right: '0',
-    bottom: '2px',
-    left: '0',
-    overflowX: 'hidden',
-    overflowY: 'scroll'
-});
-
-const MessageListContent = ({ messages }) => {
-    //Auto scroll to the end of the message list when a new message is added
-    let messagesEnd;
-    useEffect(() => {
-        messagesEnd.scrollIntoView({ behaviour: "smooth" });
-    }, [messages]);
-
-
-    return (
-        <MessageListContentContainer>
-            <Flex
-                p="24px"
-                pb="0"
-                flexFlow="row wrap"
-            >
-                <MessageList messages={messages}>
-                </MessageList>
-            </Flex>
-            <div ref={(element) => { messagesEnd = element; }}></div>
-        </MessageListContentContainer>
-    )
-}
-
-const Messenger = ({
-    messages,
-    quickReplies,
-    addUserMessage,
-    sendMessage,
-    fetchMessages
-}) => {
-
-    // addQuickReplies(content);
-
-    // const [quickReply, setQuickReply] = useState([]);
+const Messenger = ({ messages, quickReplies, addUserMessage, sendMessage, fetchMessages }) => {
 
     useEffect(() => {
+        fetchMessages();
         sendMessage({ type: 'event', value: 'Welcome' });
     }, []);
 
@@ -93,28 +34,11 @@ const Messenger = ({
             <Box>
                 <MessengerHeader />
             </Box>
-            <Box
-                position='relative'
-                height='100%'
-            >
-                <MessageListContainer>
-                    <MessageListContent messages={messages}>
-                    </MessageListContent>
-                </MessageListContainer>
+            <Box position='relative' height='100%'>
+                <MessengerWindow messages={messages} quickReplies={quickReplies} />
             </Box>
-            <Box p={3} bg='white'>
-                {quickReplies.map((quickReply, i) => {
-                    return <QuickReplies key={i} content={quickReply.content}></QuickReplies>
-                })}
-            </Box>
-            <Box
-                position='relative'
-                height="auto"
-            >
-                <MessengerInput
-                    addUserMessage={addUserMessage}
-                    sendMessage={sendMessage}
-                />
+            <Box position='relative' height="auto">
+                <MessengerInput addUserMessage={addUserMessage} sendMessage={sendMessage} />
             </Box>
         </MessengerContainer >
     );
