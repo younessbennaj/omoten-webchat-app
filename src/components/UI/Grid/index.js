3de8db444330b025fbad42600e1cf7383a20679e
Youness BENNAJ <youness.bennaj@gmail.com>
2019-08-26 17:22:06 -0400

    add grid UI component

---
+++ src/components/UI/Grid/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { GRID } from '../utils/constants';
+import Box from '../Box';
+
+const Grid = styled(Box)`
+  ${GRID};
+`
+
+Grid.defaultProps = {
+    display: 'grid'
+}
+
+export default Grid;
